cac3d323d4f8171c6ed9f8ba59b9119ff05d8ad8
HP 7TH <HP 7TH@192.168.1.3>
2020-01-02 21:17:46 +0500

    first commit

---
+++ test/src/test/Cal.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Cal {
+		
+		public Cal() {
+		
+		}
+		
+		public int sum(int num1, int num2){
+			return num1 + num2;
+		}
+		public int diff(int num1, int num2){
+			return num1 - num2;
+		}
+		public int mult(int num1, int num2){
+			return num1 * num2;
+		}
+		public int div(int num1, int num2){
+			return num1 / num2;
+		}
+}
---
+++ test/src/test/Demo.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Demo {
+
+		public static void main(String[] args) {
+			Cal calc = new Cal();
+			System.out.println(calc.sum(5, 10));
+			System.out.println(calc.diff(5, 10));
+			System.out.println(calc.div(5, 10));
+			System.out.println(calc.mult(5, 10));
+
+		}
+}
